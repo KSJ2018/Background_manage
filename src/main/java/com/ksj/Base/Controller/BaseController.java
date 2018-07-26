6027bff92201bb1e2023017026f3a3f71903c454
shiyuecamus <171378535@qq.com>
2018-07-26 16:51:54 +0800

    使用thymeleaf封装代码升级，js动态获取IP地址

--- src/main/java/com/ksj/Base/Controller/BaseController.java
+++ src/main/java/com/ksj/Base/Controller/BaseController.java
@@ -1,6 +1,5 @@
 package com.ksj.Base.Controller;
 
-import com.ksj.Base.Entity.Msg;
 import com.ksj.Utils.SecurityTool;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -24,6 +23,48 @@ public class BaseController {
         SecurityTool.setCurrentUser(null);
         return "/login";
     }
+
+    @RequestMapping("/device_type")
+    public ModelAndView device_type(Model model) {
+        ModelAndView view = new ModelAndView("DeviceType", model.asMap());
+        return view;
+    }
+    @RequestMapping("/alarm_push_manage")
+    public ModelAndView alarm_push_manage(Model model) {
+        ModelAndView view = new ModelAndView("AlarmPushManager", model.asMap());
+        return view;
+    }
+    @RequestMapping("/client_manage")
+    public ModelAndView client_manage(Model model) {
+        ModelAndView view = new ModelAndView("ClientManager", model.asMap());
+        return view;
+    }
+    @RequestMapping("/device_table")
+    public ModelAndView device_table(Model model) {
+        ModelAndView view = new ModelAndView("DeviceTable", model.asMap());
+        return view;
+    }
+    @RequestMapping("/factorys")
+    public ModelAndView factorys(Model model) {
+        ModelAndView view = new ModelAndView("Factorys", model.asMap());
+        return view;
+    }
+    @RequestMapping("/resource")
+    public ModelAndView resource(Model model) {
+        ModelAndView view = new ModelAndView("Resource", model.asMap());
+        return view;
+    }
+    @RequestMapping("/sale_manage")
+    public ModelAndView sale_manage(Model model) {
+        ModelAndView view = new ModelAndView("SaleManager", model.asMap());
+        return view;
+    }
+    @RequestMapping("/taizhang_manage")
+    public ModelAndView taizhang_manage(Model model) {
+        ModelAndView view = new ModelAndView("TaiZhangManager", model.asMap());
+        return view;
+    }
+
 
 
 }
